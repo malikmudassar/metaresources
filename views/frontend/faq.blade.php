1d40fefbd5c7a5d709bf6607cd59392223176a6f
Malik <malikmudassar@gmail.com>
2023-02-09 22:04:30 +0500

    faq

--- views/frontend/faq.blade.php
+++ views/frontend/faq.blade.php
@@ -17,7 +17,7 @@
     <div class="container">
         <div class="row">            
             <div class="col-lg-12">
-                <div class="bs-example mt-5">
+                <!-- <div class="bs-example mt-5">
                     <div class="" id="accordionExample">
                         <div class="card">
                             <div class="card-header" id="headingTwo">
@@ -92,7 +92,80 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
+                <ul class="faq-list">
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Can you explain how Prodvigate works?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">Prodvigate is very simple and effective. We promote your channel by promoting its videos on YouTube. We use Google In-feed video ads (Discovery ads) only. The ads are shown in the recommended section of YouTube, search results when someone is searching for similar content, and on the main page of YouTube’s mobile platform.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Is it legit?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">Yes. Prodvigate is a Google Partner. We were a part of the Google Partners Acceleration Program. We only use Google Ads to promote your channel. It is the only way to place your ads directly on YouTube. Our promotion is 100% legit and doesn’t violate YouTube’s terms and conditions.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Now I want to know even more about how Prodvigate works. Is it a subscription?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">No, of course not. Your promotion is set to run for a week. After the week is over, you can prolong your promotion by paying for it again. You can turn on the automatic payment option, and we will bill you automatically every time your balance is less than $1. Note: You can turn it off at any time.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Why is the promotion set to run for a week?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">Our algorithm and Google Ads algorithm need time to learn. If we spend your balance in one day, we may not get you the results you are looking for.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">And how much does it cost?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">You can start with $49. It will bring about 2,269 views. New subscribers, likes, and comments you get from the promotion are free.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">How many new subscribers can I get using Prodvigate?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">It depends on your budget and your content. The sky is the limit.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">So how can I start?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc"><ol class="faq-list__num-list"><li>First, you need to register.</li><li>Then you need to add your YouTube channel.</li><li>After this, you can choose what videos you want to promote and what targeting options you want.</li> <li>Now you can make the payment, and we will start the promotion after your videos pass the Google review, which usually takes several hours.</li></ol></div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Is it difficult to set all these settings?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">No, it’s not. But if you don’t know the settings your promotion needs, just set it to automatic. We will do all the work for you.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">But what if I want to promote one video only?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">Just select this video only, and we will promote only this video.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">What guarantees do I have?</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">Let’s see. We are a Google Partner. We don’t have bad reviews. We are officially registered, you can find the information about our company on our website. We accept payments with credit cards and PayPal, so you have double guarantees. We issue invoices. We have a very simple and transparent refund policy. We will refund you the unspent balance or the whole balance if your promotion hasn’t started yet.</div>
+                            <span class="faq-list__corner"></span>
+                        </div>
+                    </li>
+                    
+                </ul>
             </div>
         </div>
     </div>
