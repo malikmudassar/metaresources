9e3c562e04df0a1f0b4f22411feffba03df0b575
Malik <malikmudassar@gmail.com>
2023-02-09 22:25:55 +0500

    faq

--- views/frontend/faq.blade.php
+++ views/frontend/faq.blade.php
@@ -98,70 +98,70 @@
                         <h3 class="faq-list__title">What is Metapher Creator Ecosystem?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">Metapher creator ecosystem is suit of services we aim to provide to creators to unleash unlimited growth potential and bring revenue & subscribers growth.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">Is it legit?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">Yes. Metapher is a Google Partner. We were a part of the Google Partners Acceleration Program. We only use Google Ads to promote your channel. It is the only way to place your ads directly on YouTube. Our promotion is 100% legit and doesn’t violate YouTube’s terms and conditions.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">What are your Services?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">No, of course not. Your promotion is set to run for a week. After the week is over, you can prolong your promotion by paying for it again. You can turn on the automatic payment option, and we will bill you automatically every time your balance is less than $1. Note: You can turn it off at any time.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">How much does it Cost?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">Our algorithm and Google Ads algorithm need time to learn. If we spend your balance in one day, we may not get you the results you are looking for.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">How can I get Insights about my channel?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">You can start with $49. It will bring about 2,269 views. New subscribers, likes, and comments you get from the promotion are free.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">How many new subscribers can I get using Metapher?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">It depends on your budget and your content. The sky is the limit.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">So how can I start?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc"><ol class="faq-list__num-list"><li>First, you need to register.</li><li>Then you need to add your YouTube channel.</li><li>After this, you can choose what videos you want to promote and what targeting options you want.</li> <li>Now you can make the payment, and we will start the promotion after your videos pass the Google review, which usually takes several hours.</li></ol></div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">Is it difficult to set all these settings?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">No, it’s not. But if you don’t know the settings your promotion needs, just set it to automatic. We will do all the work for you.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">But what if I want to promote one video only?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">Just select this video only, and we will promote only this video.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     <li class="faq-list__item">
                         <h3 class="faq-list__title">What guarantees do I have?</h3>
                         <div class="faq-list__desc-container">
                             <div class="faq-list__desc">Let’s see. We are a Google Partner. We don’t have bad reviews. We are officially registered, you can find the information about our company on our website. We accept payments with credit cards and PayPal, so you have double guarantees. We issue invoices. We have a very simple and transparent refund policy. We will refund you the unspent balance or the whole balance if your promotion hasn’t started yet.</div>
-                            <span class="faq-list__corner"></span>
+                             
                         </div>
                     </li>
                     
