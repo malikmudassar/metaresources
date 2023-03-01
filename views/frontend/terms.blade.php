1b08d07fc605e7aa069893c6c82d650b066fea51
Malik <malikmudassar@gmail.com>
2023-03-01 10:16:30 +0500

    terms

---
+++ views/frontend/terms.blade.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en"> 
+@include('frontend.inc.head')
+<body>
+
+<!-------------- HEADER-SEC -------------------->
+@include('frontend.inc.header')
+<section class="faq-sec-hero">
+    <div class="container-fluid p-0 faq-res">
+        <h3>Terms & Conditions </h3>
+    </div>
+    <div class="faq-sec-res-content">
+        <div class="faq-label"><b>Terms & Conditions</b></div>
+    </div>
+</section>
+
+<!--------------------------------FAQ-SEC ----------------------->
+<section class="FAQ-sec">
+    <div class="container">
+        <div class="row">            
+            <div class="col-lg-12">
+                
+                <ul class="faq-list">
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Introduction</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                These terms and conditions (the "Terms") govern your use of the Metapher.in 
+                                website and the services offered through it (the "Services"). 
+                                By accessing or using the Services, you agree to be bound by these Terms. 
+                                If you do not agree to these Terms, you may not use the Services.
+                            </div>
+                             
+                        </div>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in reserves the right to modify these Terms at any 
+                                time, effective upon posting of the modified Terms on the 
+                                website. Your continued use of the Services after any such 
+                                changes shall be deemed to be your acceptance of the new terms.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Services</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in provides social media management solutions to its users. 
+                                The Services are designed to help users manage their social media 
+                                presence, and Metapher.in provides a variety of tools to help users 
+                                achieve this goal.
+                            </div>
+                        </div>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in does not guarantee that the Services will be available
+                                 at all times, or that access to the Services will be 
+                                 uninterrupted or error-free. The Services may be temporarily 
+                                 unavailable for maintenance or upgrades, or due to technical 
+                                 problems beyond the control of Metapher.in.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Use of Personal Information</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in uses personal information for the purpose of providing the 
+                                Services, including processing payments, and for the purpose of 
+                                improving and enhancing the Services.
+                            </div>
+                            <div class="faq-list__desc">
+                                Metapher.in may also use personal information to send you 
+                                notifications and updates about the Services, and for 
+                                other marketing purposes.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">User Account</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                To access and use the Services, you must create an account with Metapher.in by providing accurate and complete information. You are responsible for maintaining the confidentiality of your account information, and you must not allow others to access your account.
+
+                            </div>
+                        </div>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                You agree to immediately notify Metapher.in of any unauthorized use of your account or any other breach of security.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Content</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                The Services allow you to post, upload, publish, or otherwise transmit content to the website (the "User Content"). You retain all rights in the User Content, and you are solely responsible for the User Content that you make available through the Services.
+                            </div>
+                        </div>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                By making any User Content available through the Services, you grant Metapher.in a non-exclusive, transferable, sub-licensable, royalty-free, worldwide license to use, copy, modify, and display the User Content in connection with the Services.
+
+                            </div>
+                        </div>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                3 You represent and warrant that you have all rights necessary to grant the rights granted in this section, and that the User Content does not infringe any intellectual property rights or violate any laws.
+
+                            </div>
+                        </div>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in reserves the right, in its sole discretion, to remove or refuse to post any User Content for any reason, including User Content that violates these Terms.
+
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Proprietary Rights</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                The Services and all intellectual property rights, including without limitation patents, trademarks, copyrights, and trade secrets, in the Services and any modifications or derivatives thereof are the exclusive property of Metapher.in.
+
+                            </div>
+                        </div>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                You agree not to copy, modify, distribute, sell, or lease any part of the Services, or reverse engineer or attempt to extract the source code of any software provided through the Services, except as expressly permitted by applicable law
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Disclaimer of Warranties</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                            THE SERVICES ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY WARRANTY FOR INFORMATION, SERVICES, UNINTERRUPTED ACCESS, OR PRODUCTS PROVIDED THROUGH OR IN CONNECTION WITH THE SERVICES, INCLUDING WITHOUT LIMITATION THE SOFTWARE LICENSED TO YOU AND THE RESULTS OBTAINED
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+ </section>
+
+<!---------------------- NEWS LETTERS ---------------------->
+@include('frontend.inc.newsletter')
+<!------------------- FOOTER-SEC ---------------------->
+
+@include('frontend.inc.footer')
