27a30ac5f4cdae522f5a7af499d5cf3639e36c1e
Malik <malikmudassar@gmail.com>
2023-03-01 09:03:09 +0500

    privacy blade

---
+++ views/frontend/privacy.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en"> 
+@include('frontend.inc.head')
+<body>
+
+<!-------------- HEADER-SEC -------------------->
+@include('frontend.inc.header')
+<section class="faq-sec-hero">
+    <div class="container-fluid p-0 faq-res">
+        <h3>Privacy Policy </h3>
+    </div>
+    <div class="faq-sec-res-content">
+        <div class="faq-label"><b>Privacy Policy</b></div>
+    </div>
+</section>
+
+<!--------------------------------FAQ-SEC ----------------------->
+<section class="FAQ-sec">
+    <div class="container">
+        <div class="row">            
+            <div class="col-lg-12">
+                
+                <ul class="faq-list">
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Introduction</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in is committed to protecting the privacy of its users 
+                                and customers. This Privacy Policy sets out how Metapher.in 
+                                collects, uses, and protects personal information.
+                            </div>
+                             
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Collection of Personal Information</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in collects personal information when you create an account, 
+                                purchase the Services, or otherwise interact with the website. 
+                                The personal information collected may include your name, email address, 
+                                payment information, and other information necessary to provide the 
+                                Services.
+                            </div>
+                            <div class="faq-list__desc">
+                                Metapher.in may also collect information about your use of the Services, 
+                                including the content you upload or post to the website, and other 
+                                information related to your use of the Services.
+                            </div>
+                             
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Use of Personal Information</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in uses personal information for the purpose of providing the 
+                                Services, including processing payments, and for the purpose of 
+                                improving and enhancing the Services.
+                            </div>
+                            <div class="faq-list__desc">
+                                Metapher.in may also use personal information to send you 
+                                notifications and updates about the Services, and for 
+                                other marketing purposes.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Sharing of Personal Information</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in does not sell, trade, or otherwise transfer personal 
+                                information to outside parties.
+                            </div>
+                            <div class="faq-list__desc">
+                                Metapher.in may disclose personal information if required to 
+                                do so by law, or if it believes in good faith that such 
+                                disclosure is necessary to protect the rights of Metapher.in 
+                                or others, or to respond to a court order, subpoena, 
+                                or other legal process.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Security of Personal Information</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in takes reasonable measures to protect the security of personal 
+                                information, including the use of encryption and secure servers. 
+                                However, no data transmission over the Internet can be guaranteed 
+                                to be 100% secure, and Metapher.in cannot guarantee the security 
+                                of personal information.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Access to Personal Information</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                You have the right to access and correct your personal information, 
+                                and you can do so by logging into your account on the website.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Changes to Privacy Policy</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                Metapher.in reserves the right to modify this Privacy Policy at any time, 
+                                effective upon posting of the modified Privacy Policy on the website. 
+                                Your continued use of the Services after any such changes shall be deemed 
+                                to be your acceptance of the new terms.
+                            </div>
+                        </div>
+                    </li>
+                    <li class="faq-list__item">
+                        <h3 class="faq-list__title">Contact Information</h3>
+                        <div class="faq-list__desc-container">
+                            <div class="faq-list__desc">
+                                If you have any questions or concerns about this Privacy Policy, 
+                                please contact Metapher.in at contact@metapher.in
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+ </section>
+
+<!---------------------- NEWS LETTERS ---------------------->
+@include('frontend.inc.newsletter')
+<!------------------- FOOTER-SEC ---------------------->
+
+@include('frontend.inc.footer')
