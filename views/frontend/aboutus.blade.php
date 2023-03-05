17d23e4afc4dcd33cd6a15ba54e28ae8ec678767
Malik <malikmudassar@gmail.com>
2023-03-05 14:38:33 +0500

    about us blade

--- views/frontend/aboutus.blade.php
+++ views/frontend/aboutus.blade.php
@@ -9,7 +9,7 @@
 <!---------------------- Blog SEC -------------------->
 <section class="container py-5">
   <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-7">
       <h2 class="mb-4">Our Story</h2>
       <h5>We’re strategists, growth experts, marketers, analysts, and what not</h5>
       <p>We have over five years of experience in the creative industry, 
@@ -18,7 +18,7 @@
       <p>Make growth highly accessible to content creators across the globe, 
         to take content beyond linguistic barriers, to take the media-tech industry by storm.</p>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-5">
       <div class="card">
         <img src="public/markup/img/ceo.png" class="card-img-top" alt="CEO photo">
         <div class="card-footer">
