51b8c09308b02e85b52d8118247ee62b5e1fc00c
Malik <malikmudassar@gmail.com>
2023-03-05 14:43:03 +0500

    about us blade

--- views/frontend/aboutus.blade.php
+++ views/frontend/aboutus.blade.php
@@ -7,27 +7,30 @@
 @include('frontend.inc.header')
 
 <!---------------------- Blog SEC -------------------->
-<section class="container py-5">
-  <div class="row">
-    <div class="col-md-7">
-      <h2 class="mb-4">Our Story</h2>
-      <h5>We’re strategists, growth experts, marketers, analysts, and what not</h5>
-      <p>We have over five years of experience in the creative industry, 
-        promoting and optimizing content for creators that is as good as it grows.</p>
-      <h2 class="mt-5 mb-4">Our Vision</h2>
-      <p>Make growth highly accessible to content creators across the globe, 
-        to take content beyond linguistic barriers, to take the media-tech industry by storm.</p>
-    </div>
-    <div class="col-md-5">
-      <div class="card">
-        <img src="public/markup/img/ceo.png" class="card-img-top" alt="CEO photo">
-        <div class="card-footer">
-          <h4 class="card-title">Sarah Elixir</h4>
-          <p class="card-text">CEO</p>
+<section class="sec-vision">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-7">
+            <h2 class="mb-4">Our Story</h2>
+            <h5>We’re strategists, growth experts, marketers, analysts, and what not</h5>
+            <p>We have over five years of experience in the creative industry, 
+                promoting and optimizing content for creators that is as good as it grows.</p>
+            <h2 class="mt-5 mb-4">Our Vision</h2>
+            <p>Make growth highly accessible to content creators across the globe, 
+                to take content beyond linguistic barriers, to take the media-tech industry by storm.</p>
+            </div>
+            <div class="col-md-5">
+            <div class="card">
+                <img src="public/markup/img/ceo.png" class="card-img-top" alt="CEO photo">
+                <div class="card-footer">
+                <h4 class="card-title">Sarah Elixir</h4>
+                <p class="card-text">CEO</p>
+                </div>
+            </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+  
 </section>
 <!------------------- FOOTER-SEC ---------------------->
 
