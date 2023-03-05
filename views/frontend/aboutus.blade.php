6b1f1386e4324d61ae520cd2ec47999bc24e4d4f
Malik <malikmudassar@gmail.com>
2023-03-05 14:29:16 +0500

    aboutus page

--- views/frontend/aboutus.blade.php
+++ views/frontend/aboutus.blade.php
@@ -5,16 +5,29 @@
 
 <!-------------- HEADER-SEC -------------------->
 @include('frontend.inc.header')
-<!-------------------------- HERO-SEC -------------------->
-@include('frontend.inc.hero')
+
 <!---------------------- Blog SEC -------------------->
-<section class="blog-sec">
-    <div class="container" style="text-align:center">
-        <h1>About us! Coming Soon</h1>
-        <div class="row">
-            
-        </div>
+<section class="container py-5">
+  <div class="row">
+    <div class="col-md-6">
+      <h2 class="mb-4">Our Story</h2>
+      <h5>We’re strategists, growth experts, marketers, analysts, and what not</h5>
+      <p>We have over five years of experience in the creative industry, 
+        promoting and optimizing content for creators that is as good as it grows.</p>
+      <h2 class="mt-5 mb-4">Our Vision</h2>
+      <p>Make growth highly accessible to content creators across the globe, 
+        to take content beyond linguistic barriers, to take the media-tech industry by storm.</p>
     </div>
+    <div class="col-md-6">
+      <div class="card">
+        <img src="public/markup/img/ceo.png" class="card-img-top" alt="CEO photo">
+        <div class="card-footer">
+          <h4 class="card-title">Sarah Elixir</h4>
+          <p class="card-text">CEO</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 <!------------------- FOOTER-SEC ---------------------->
 
