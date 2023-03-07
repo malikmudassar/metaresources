af33c0a0c33b58bb33c4597b34bb79cc32e7008e
Malik <malikmudassar@gmail.com>
2023-03-07 14:14:16 +0500

    changes

--- views/frontend/blog.blade.php
+++ views/frontend/blog.blade.php
@@ -11,7 +11,7 @@
 <section class="dark" style="background: #3c3b40;
     padding-top: 50px;
     padding-bottom: 50px;">
-	
+	<div class="b-head"><img src="public/markup/img/blog-background.jpg"></div>
 </section>
 <!------------------- FOOTER-SEC ---------------------->
 
@@ -19,5 +19,12 @@
 <style>
    .dark h1 h2 h3 h4 span p {
         color: white;
+   }
+   .b-head {
+        min-height: 200px;
+
+   }
+   .b-head img {
+        object-fit: cover;
    }
 </style>
