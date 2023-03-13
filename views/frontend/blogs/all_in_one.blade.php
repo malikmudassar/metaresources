67c9272b98c3484302a3cf8b6cf7d402a5919682
Malik <malikmudassar@gmail.com>
2023-03-13 09:16:37 +0500

    changes in blog

---
+++ views/frontend/blogs/all_in_one.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en"> 
+@include('frontend.inc.head')
+
+<body>
+
+<!-------------- HEADER-SEC -------------------->
+@include('frontend.inc.header')
+
+<!---------------------- Blog SEC -------------------->
+<section class="dark" style="background: #3c3b40;">
+	<div class="b-head">
+        <img src="public/markup/img/blog-background.jpg" alt="">
+    </div>
+    <div class="p-5 posts">
+        <div class="row" style="padding-bottom:50px !important">
+            <div class="col-md-8">
+                <div class="">
+                    <img src="public/markup/img/img-pl.jpg">
+                </div>
+                <h3>Meta Growth Package: The All-In-One Tool For YouTube Content Creators</h3>
+                <p>
+                    As a content creator on YouTube, one of the biggest challenges you face is getting your content noticed by a wider audience. With millions of videos being uploaded every day, it can be difficult to stand out and attract subscribers. However, with the help of <a href="https://metapher.in/meta-creator#meta-growth-sec" target="_blank">Meta Growth Package</a>, you can overcome these challenges and grow your YouTube channel faster.
+                </p>
+                <p>
+                    Meta Growth Package offers 14+ services that are designed to help you grow your YouTube channel. One of the key services that the package includes is a Personal Certified Channel Manager who will assist you at every stage of your YouTube journey. This manager will help you identify growth opportunities and provide you with personalised advice on how to optimise your content for maximum engagement.
+                </p>
+                <p>
+                    Another service offered by Meta Growth Package is the unique Channel Development Program, which is designed to help content creators understand the algorithms that govern YouTube. By understanding these algorithms, you can create content that is more likely to be recommended to viewers, increasing your reach and visibility.
+                </p>
+                <p>
+                    In addition to these services, Meta Growth Package also offers a range of other tools and resources that can help you grow your YouTube channel. These include keyword research, competitor analysis, and audience targeting, all of which are designed to help you create content that resonates with your target audience.
+                </p>
+                <p>
+                    It is an invaluable resource for YouTube content creators who want to overcome growth-related challenges and grow their channels faster. With a Personal Certified Channel Manager to guide you, and a range of tools and resources to help you optimise your content, you can be confident that your YouTube journey will be a success. So why wait? Sign up for Meta Growth Package today and take your YouTube channel to the next level!
+                </p>
+                <div class="mt-5">
+                    Keywords: 
+                    <span class="label label-default">Metapher</span>
+                    <span class="label label-default">Meta Growth Package</span>
+                    <span class="label label-default">YouTube</span>
+                    <span class="label label-default">Growth Content</span>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="popular">
+                    <h3>MOST POPULAR</h3>
+                    <hr>
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Revolutionizing Video Translation: How Metapher Is Meeting The Demands Of The Digital Age</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/023.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Meta Growth Package: The All-In-One Tool For YouTube Content Creators</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Latest Updates By YouTube: What You Need To Know</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@include('frontend.inc.newsletter')
+<!------------------- FOOTER-SEC ---------------------->
+
+@include('frontend.inc.footer')
+<style>
+   .dark h1 h2 h3 h4 span p {
+        color: white;
+   }
+   .b-head {
+        height: 350px;
+   }
+   .b-head img {
+        height: 100%;
+   }
+   .posts {
+    background: #fff;
+   }
+   .post {
+    min-height: 250px;
+    margin-bottom:20px;
+    background-color: white;
+    padding: 10px;
+    box-shadow: 15px 16px 17px #cdcdcd;
+   }
+   .popular {
+    min-height: 500px; 
+   }
+   .post__img img {
+    height:180px;
+   }
+   .post__content {
+        width:60%;
+        text-align:justify;
+        padding:10px;
+   }
+   .post__content p {
+        font-size:12px;
+        color: #6c5a5a;
+   }
+    .post__img,
+    .post__content {
+        display: inline-block;
+        vertical-align: top; /* This is to ensure both divs are aligned at the top */
+    }
+    hr {width: 100%}
+    .paragraph {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 5;
+        overflow: hidden;
+    }
+    .post__footer {
+        font-size: 12px;
+        color: #6c5a5a;
+        bottom: 10px; 
+        border-top: 0.5px dotted #e5d5d5;
+    }
+    .post {
+        position: relative;
+    }
+
+    .post__footer {
+        position: absolute;
+        bottom: 13px;
+        padding-top: 10px;
+        width: 59%;
+    }
+    .card {
+        border-radius: 0px !important;
+    }
+    .mp-card {
+        border: 0.5px dotted #e5d5d5;
+        box-shadow: 15px 16px 17px #cdcdcd;
+        border-radius: none;
+    }
+    p {
+        text-align: justify;
+        font-size: 14px;
+        padding-top: 10px; 
+        padding-bottom: 10x;
+    }
+    .label-default {
+        background: black;
+        border-radius:3px;
+        padding: 2px;
+    }
+</style>
