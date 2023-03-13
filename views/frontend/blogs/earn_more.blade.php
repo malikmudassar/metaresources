fc4eacf5624333c93ecce1c507d13ee2ea1362ae
Malik <malikmudassar@gmail.com>
2023-03-13 11:23:47 +0500

    changes in blog

---
+++ views/frontend/blogs/earn_more.blade.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en"> 
+@include('frontend.inc.head')
+
+<body>
+
+<!-------------- HEADER-SEC -------------------->
+@include('frontend.inc.header')
+
+<!---------------------- Blog SEC -------------------->
+<section class="dark" style="background: #3c3b40;">
+	<div class="b-head">
+        <img src="public/markup/img/blog-background.jpg" alt="">
+    </div>
+    <div class="p-5 posts">
+        <div class="row" style="padding-bottom:50px !important">
+            <div class="col-md-8">
+                <div class="">
+                    <img src="public/markup/img/img-pl.jpg">
+                </div>
+                <h3>Beyond YouTube Monetization: How Meta Income Can Help You Earn More.</h3>
+                <p>
+                    If you're a YouTube content creator, you know that earning a significant income from your content can be challenging. While YouTube provides monetization options, such as advertising revenue and sponsorships, these methods may not always be sufficient to generate a stable income stream. This is where Meta Income comes in – an innovative platform that can help you increase your revenue with minimal effort.
+                </p>
+                <p>
+                    Meta Income offers 12+ income services, including managing Content ID, cross-platform content distribution, and developing mobile apps and games. These services can help you reach a wider audience and monetize your content more effectively. For example, Content ID allows you to claim your content and receive a portion of the revenue generated from ads placed on videos that use your content. Cross-platform content distribution allows you to reach audiences on multiple platforms, such as TikTok, Instagram, and Facebook, and earn revenue from those platforms.
+                </p>
+                <p>
+                    Developing mobile apps and games is another way to generate revenue with Meta Income. You can create apps and games that feature your brand or content and earn revenue from in-app purchases and ads. This is a great way to diversify your income stream and reach a new audience.
+                </p>
+                <p>
+                    Meta Income's Affiliate Business Partner Program is another way to increase your revenue. As a partner, you'll have access to more brand deals and sponsorship opportunities. This means you can earn more money from sponsored content and collaborations.
+                </p>
+                <p>
+                    So, how can Meta Income help you increase your revenue as a YouTube content creator? Here are a few ways: <br><br>
+                    <b>Diversify your income stream:</b> By using Meta Income's 12+ income services, you can diversify your income stream and reduce your reliance on YouTube monetization options. This can help you generate a more stable and consistent income.<br><br>
+                    <b>Reach a wider audience:</b> With Meta Income's cross-platform content distribution, you can reach audiences on multiple platforms and increase your visibility. This can help you attract more sponsors and brand deals.<br><br>
+                    <b>Monetize your content more effectively</b>: With Content ID and other income services, you can monetize your content more effectively and earn revenue from multiple sources.
+                </p>
+                <p>
+                    Meta Income is an innovative platform that can help YouTube content creators increase their revenue with minimal effort. By using Meta Income's income services, content creators can diversify their income stream, reach a wider audience, and monetize their content more effectively. If you're a YouTube content creator looking to increase your revenue, Meta Income is definitely worth considering.
+                </p>
+                <div class="mt-5">
+                    Keywords: 
+                    <span class="label label-default">Metapher</span>
+                    <span class="label label-default">Meta Growth Package</span>
+                    <span class="label label-default">YouTube</span>
+                    <span class="label label-default">Growth Content</span>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="popular">
+                    <h3>MOST POPULAR</h3>
+                    <hr>
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Revolutionizing Video Translation: How Metapher Is Meeting The Demands Of The Digital Age</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/023.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Meta Growth Package: The All-In-One Tool For YouTube Content Creators</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Latest Updates By YouTube: What You Need To Know</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@include('frontend.inc.newsletter')
+<!------------------- FOOTER-SEC ---------------------->
+
+@include('frontend.inc.footer')
+<style>
+   .dark h1 h2 h3 h4 span p {
+        color: white;
+   }
+   .b-head {
+        height: 350px;
+   }
+   .b-head img {
+        height: 100%;
+   }
+   .posts {
+    background: #fff;
+   }
+   .post {
+    min-height: 250px;
+    margin-bottom:20px;
+    background-color: white;
+    padding: 10px;
+    box-shadow: 15px 16px 17px #cdcdcd;
+   }
+   .popular {
+    min-height: 500px; 
+   }
+   .post__img img {
+    height:180px;
+   }
+   .post__content {
+        width:60%;
+        text-align:justify;
+        padding:10px;
+   }
+   .post__content p {
+        font-size:12px;
+        color: #6c5a5a;
+   }
+    .post__img,
+    .post__content {
+        display: inline-block;
+        vertical-align: top; /* This is to ensure both divs are aligned at the top */
+    }
+    hr {width: 100%}
+    .paragraph {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 5;
+        overflow: hidden;
+    }
+    .post__footer {
+        font-size: 12px;
+        color: #6c5a5a;
+        bottom: 10px; 
+        border-top: 0.5px dotted #e5d5d5;
+    }
+    .post {
+        position: relative;
+    }
+
+    .post__footer {
+        position: absolute;
+        bottom: 13px;
+        padding-top: 10px;
+        width: 59%;
+    }
+    .card {
+        border-radius: 0px !important;
+    }
+    .mp-card {
+        border: 0.5px dotted #e5d5d5;
+        box-shadow: 15px 16px 17px #cdcdcd;
+        border-radius: none;
+    }
+    p {
+        text-align: justify;
+        font-size: 14px;
+        padding-top: 10px; 
+        padding-bottom: 10x;
+    }
+    .label-default {
+        background: black;
+        border-radius:3px;
+        padding: 2px;
+    }
+</style>
