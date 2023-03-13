67a9793ccfa5892f1f76b1207f750a0d9ed2eef6
Malik <malikmudassar@gmail.com>
2023-03-13 08:47:20 +0500

    blog posts

---
+++ views/frontend/blogs/revolution.blade.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en"> 
+@include('frontend.inc.head')
+
+<body>
+
+<!-------------- HEADER-SEC -------------------->
+@include('frontend.inc.header')
+
+<!---------------------- Blog SEC -------------------->
+<section class="dark" style="background: #3c3b40;">
+	<div class="b-head">
+        <img src="public/markup/img/blog-background.jpg" alt="">
+    </div>
+    <div class="p-5 posts">
+        <div class="row" style="padding-bottom:50px !important">
+            <div class="col-md-8">
+                <div class="">
+                    <img src="public/markup/img/img-pl.jpg">
+                </div>
+                <h3>Revolutionizing Video Translation: How Metapher Is Meeting The Demands Of The Digital Age</h3>
+                <p>In today's digital age, video content has become increasingly popular across various platforms, and the demand for video translation services has skyrocketed. Video translation is essential for creators, businesses, and organisations that want to expand their audience and reach out to a global market. However, finding a reliable and customised video translation service can be a challenging task.</p>
+                <p>
+                    The problem with traditional video translation services is that they provide a one-size-fits-all solution, which often results in inaccurate translations and a loss of the original message's intent. Furthermore, most services don't provide an all-in-one solution, which can lead to a fragmented video production process, making it challenging to meet tight deadlines and create a coherent final product.
+                </p>
+                <p>
+                    This is where Metapher comes in. As a leading video translation service, Metapher offers a customised approach to video translation that allows creators and businesses to expand their audience and increase their revenue exponentially. Our services include subtitles, voice-overs, video editing, and distribution, all under one roof, ensuring a seamless video production process
+                </p>
+                <p>
+                    Our customised approach starts with a thorough understanding of the client's needs, target audience, and desired outcomes. We work closely with our clients to ensure that their videos are translated accurately, retaining the original message's intent while making it culturally relevant to the target audience
+                </p>
+                <p>
+                    Our team of experienced translators and voice-over artists ensures that the translations are not only accurate but also natural-sounding and engaging. We also provide video editing services to enhance the visual experience and make it more engaging for the target audience
+                </p>
+                <p>
+                    At Metapher, we understand that time is of the essence in the video production process. That's why we offer a fast turnaround time, ensuring that our clients can meet their deadlines and maintain their video production schedule
+                </p>
+                <p>
+                    Our distribution services ensure that the translated videos reach the target audience across various platforms, including social media, YouTube, Vimeo, and other online video platforms.
+                </p>
+                <p>
+                    Many creators and businesses worldwide have used our services to gain millions of views, expand their audience, and increase their revenue. We pride ourselves on our ability to provide high-quality video translation services that deliver measurable results
+                </p>
+                <p>
+                    If you're a creator or a business looking for a reliable and customised video translation service, Metapher is the one-stop solution for all your video translation needs. Our approach ensures accurate translations, natural-sounding voice-overs, engaging visuals, and fast turnaround times, ensuring that you can expand your audience and increase your revenue exponentially
+                </p>
+                Keywords: 
+                <span class="label label-default">Metapher</span>
+                <span class="label label-default">Meta translate</span>
+                <span class="label label-default">YouTube</span>
+                <span class="label label-default">Video Content</span>
+            </div>
+            <div class="col-md-4">
+                <div class="popular">
+                    <h3>MOST POPULAR</h3>
+                    <hr>
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Revolutionizing Video Translation: How Metapher Is Meeting The Demands Of The Digital Age</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/023.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Meta Growth Package: The All-In-One Tool For YouTube Content Creators</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 mb-4">
+                        <div class="card mp-card">
+                            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                                <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Latest Updates By YouTube: What You Need To Know</h5>
+                                
+                                <a href="#!" class="btn btn-warning btn-block">Read</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@include('frontend.inc.newsletter')
+<!------------------- FOOTER-SEC ---------------------->
+
+@include('frontend.inc.footer')
+<style>
+   .dark h1 h2 h3 h4 span p {
+        color: white;
+   }
+   .b-head {
+        height: 350px;
+   }
+   .b-head img {
+        height: 100%;
+   }
+   .posts {
+    background: #fff;
+   }
+   .post {
+    min-height: 250px;
+    margin-bottom:20px;
+    background-color: white;
+    padding: 10px;
+    box-shadow: 15px 16px 17px #cdcdcd;
+   }
+   .popular {
+    min-height: 500px; 
+   }
+   .post__img img {
+    height:180px;
+   }
+   .post__content {
+        width:60%;
+        text-align:justify;
+        padding:10px;
+   }
+   .post__content p {
+        font-size:12px;
+        color: #6c5a5a;
+   }
+    .post__img,
+    .post__content {
+        display: inline-block;
+        vertical-align: top; /* This is to ensure both divs are aligned at the top */
+    }
+    hr {width: 100%}
+    .paragraph {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 5;
+        overflow: hidden;
+    }
+    .post__footer {
+        font-size: 12px;
+        color: #6c5a5a;
+        bottom: 10px; 
+        border-top: 0.5px dotted #e5d5d5;
+    }
+    .post {
+        position: relative;
+    }
+
+    .post__footer {
+        position: absolute;
+        bottom: 13px;
+        padding-top: 10px;
+        width: 59%;
+    }
+    .card {
+        border-radius: 0px !important;
+    }
+    .mp-card {
+        border: 0.5px dotted #e5d5d5;
+        box-shadow: 15px 16px 17px #cdcdcd;
+        border-radius: none;
+    }
+</style>
