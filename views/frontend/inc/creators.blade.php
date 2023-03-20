6233702bad59c748c69e3402cd6ed517d7d28e97
Malik <malikmudassar@gmail.com>
2023-03-20 16:54:52 +0500

    landing page

--- views/frontend/inc/creators.blade.php
+++ views/frontend/inc/creators.blade.php
@@ -2,23 +2,23 @@
     <div class="container">
         <h5>Creators We’ve Worked With</h5>
         <div class="row">
-            <div class="offset-2"></div>
-            <div class="col-md-2">
+            <div class="offset-5"></div>
+            <div class="col-md-1">
                 <div class="boxesWrap">
                     <img src="public/markup/img/1.jpg" alt="">
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-1">
                 <div class="boxesWrap">
                     <img src="public/markup/img/2.jpg" alt="">
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-1">
                 <div class="boxesWrap">
                     <img src="public/markup/img/3.jpg" alt="">
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-1">
                 <div class="boxesWrap">
                     <img src="public/markup/img/4.jpg" alt="">
                 </div>
