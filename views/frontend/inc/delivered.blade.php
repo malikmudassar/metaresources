d6b978dced52fe2afa8e25e926d6a0806656c678
Malik <malikmudassar@gmail.com>
2023-03-21 09:45:09 +0500

    delivered section

--- views/frontend/inc/delivered.blade.php
+++ views/frontend/inc/delivered.blade.php
@@ -24,18 +24,15 @@
                             </div>
                             <div class="col">
                                 <div class="deliveredCardimg">
-                                    <!-- <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M8.39 18.49V8.32998C8.39 7.92998 8.51 7.53998 8.73 7.20998L11.46 3.14998C11.89 2.49998 12.96 2.03998 13.87 2.37998C14.85 2.70998 15.5 3.80997 15.29 4.78997L14.77 8.05998C14.73 8.35998 14.81 8.62998 14.98 8.83998C15.15 9.02998 15.4 9.14997 15.67 9.14997H19.78C20.57 9.14997 21.25 9.46997 21.65 10.03C22.03 10.57 22.1 11.27 21.85 11.98L19.39 19.47C19.08 20.71 17.73 21.72 16.39 21.72H12.49C11.82 21.72 10.88 21.49 10.45 21.06L9.17 20.07C8.68 19.7 8.39 19.11 8.39 18.49Z"
                                             fill="#E6AA5A" />
                                         <path
                                             d="M5.21 6.38H4.18C2.63 6.38 2 6.98 2 8.46V18.52C2 20 2.63 20.6 4.18 20.6H5.21C6.76 20.6 7.39 20 7.39 18.52V8.46C7.39 6.98 6.76 6.38 5.21 6.38Z"
                                             fill="#E6AA5A" />
-                                    </svg> -->
-                                    <iframe src="https://www.youtube.com/watch?v=0Hafj-1sttk" 
-                                    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
-                                    clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                        
+                                    </svg>
+                                    
                                 </div>
                                 <div class="delivered_outer_text">
                                     <h5 class="card-title">456+</h5>
@@ -44,14 +41,11 @@
                             </div>
                             <div class="col">
                                 <div class="deliveredCardimg">
-                                    <!-- <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M8 13.1739C12.3386 13.1739 16 13.8789 16 16.599C16 19.32 12.3146 20 8 20C3.66237 20 0 19.295 0 16.575C0 13.8539 3.68538 13.1739 8 13.1739ZM8 0C10.9391 0 13.294 2.35402 13.294 5.29105C13.294 8.22808 10.9391 10.5831 8 10.5831C5.0619 10.5831 2.70601 8.22808 2.70601 5.29105C2.70601 2.35402 5.0619 0 8 0Z"
                                             fill="#E6AA5A" />
-                                    </svg> -->
-                                    <iframe src="https://www.youtube.com/watch?v=0Hafj-1sttk" 
-                                    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
-                                    clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    </svg>
                                 </div>
                                 <div class="delivered_outer_text">
                                     <h5 class="card-title">1456+</h5>
