f54f1bae185c3670e69e3dff095c2e58b2edaf04
Malik <malikmudassar@gmail.com>
2023-03-22 15:26:56 +0500

    faq landing

--- views/frontend/inc/faq.blade.php
+++ views/frontend/inc/faq.blade.php
@@ -21,43 +21,55 @@
                               </div>
                               <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
                                   <div class="card-body">
-                                      <p>Metapher creator ecosystem is suit of services we aim to provide to creators to unleash unlimited growth potential and bring revenue & subscribers growth.</p>
+                                        <p>
+                                        Metapher Creator Ecosystem offers tools and services we provide to creators so that We can unleash their unlimited potential and help them with organic growth and revenue generation.
+                                        </p>
                                   </div>
                               </div>
                           </div>
                           <div class="card">
                               <div class="card-header" id="headingOne">
                                   <h4 class="mb-0">
-                                      <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne">What are your Services?<i class="fa fa-plus"></i></button>									
+                                        <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne">
+                                            What are your Products?<i class="fa fa-plus"></i>
+                                        </button>									
                                   </h4>
                               </div>
                               <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                                   <div class="card-body">
-                                    <p>Metapher creator ecosystem is suit of services we aim to provide to creators to unleash unlimited growth potential and bring revenue & subscribers growth.</p>
+                                        <p>
+                                            Our products include Meta Creators, Meta Translate and MetaDb. For more details, refer to our website
+                                        </p>
                                   </div>
                               </div>
                           </div>
                           <div class="card">
                               <div class="card-header" id="headingThree">
                                   <h4 class="mb-0">
-                                      <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree">How much does it Cost?<i class="fa fa-plus" aria-hidden="true"></i></button>                     
+                                        <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree">
+                                            How much does it Cost?<i class="fa fa-plus" aria-hidden="true"></i>
+                                        </button>                     
                                   </h4>
                               </div>
                               <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                                   <div class="card-body">
-                                    <p>Metapher creator ecosystem is suit of services we aim to provide to creators to unleash unlimited growth potential and bring revenue & subscribers growth.</p>
+                                        <p>
+                                            We offer customised pricing based on the requirements of clients.
+                                        </p>
                                   </div>
                               </div>
                           </div>
                           <div class="card">
                             <div class="card-header" id="headingFour">
                                 <h4 class="mb-0">
-                                    <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour">Any content creator you have Worked with?<i class="fa fa-plus" aria-hidden="true"></i></button>                     
+                                    <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour">
+                                        Any content creator you have Worked with?<i class="fa fa-plus" aria-hidden="true"></i>
+                                    </button>                     
                                 </h4>
                             </div>
                             <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
                                 <div class="card-body">
-                                    <p>Metapher creator ecosystem is suit of services we aim to provide to creators to unleash unlimited growth potential and bring revenue & subscribers growth.</p>
+                                    <p>Some of the creators we’ve worked with include Skin Inspired, Suraj Film                   Foundation, Sur Classical, Kamaldeep Music, Ash Pahwa, Namak Samak, Yummy  Food with Ayra and NatureClap Mushrooms. You can be our next.</p>
                                 </div>
                             </div>
                         </div>
@@ -69,7 +81,7 @@
                             </div>
                             <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
                                 <div class="card-body">
-                                    <p>Metapher creator ecosystem is suit of services we aim to provide to creators to unleash unlimited growth potential and bring revenue & subscribers growth.</p>
+                                    <p>You can simply get started by signing up with your Gmail associated with the YouTube channel. After you sign-up we will verify your account and then you can select from our wide-range of our services catering to your needs.</p>
                                 </div>
                             </div>
                         </div>
@@ -81,7 +93,7 @@
                             </div>
                             <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
                                 <div class="card-body">
-                                    <p>Metapher creator ecosystem is suit of services we aim to provide to creators to unleash unlimited growth potential and bring revenue & subscribers growth.</p>
+                                    <p>Metapher Creator offers YouTube Channel Audits and Channel Development Programs, which can provide you with insights and feedback on your channel's performance. You can also reach out to our one-to-one personal assistance for further guidance and support.</p>
                                 </div>
                             </div>
                         </div>
