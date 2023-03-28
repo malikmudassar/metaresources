8ca8ed4762637df1e9da9b522bf99f7c024323ae
Malik <malikmudassar@gmail.com>
2023-03-28 14:29:56 +0500

    hero

--- views/frontend/inc/hero.blade.php
+++ views/frontend/inc/hero.blade.php
@@ -9,7 +9,20 @@
                 </div>
                 <div class="heroForms">
                     <form action="" class="herofieds d-flex">                        
-                        <button type="button" class="btn-primary">Become a Meta Creator</button>
+                        <!-- <button type="button" class="btn-primary">Become a Meta Creator</button> -->
+                        <div class="input-with-button">
+                            <label class="visually-hidden" for="hero-enter-channel">Enter YouTube channel name or URL</label>
+                            <div class="input-with-button__input-container">
+                                <input name="query" class="input-with-button__input " type="text" id="hero-enter-channel" placeholder="Enter YouTube channel name or URL" autocomplete="off">
+                                <button class="input-with-button__cancel" type="button">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 19.5C16.4183 19.5 20 15.9183 20 11.5C20 7.08172 16.4183 3.5 12 3.5C7.58172 3.5 4 7.08172 4 11.5C4 15.9183 7.58172 19.5 12 19.5ZM9.70711 7.79289C9.31658 7.40237 8.68342 7.40237 8.29289 7.79289C7.90237 8.18342 7.90237 8.81658 8.29289 9.20711L10.5858 11.5L8.29289 13.7929C7.90237 14.1834 7.90237 14.8166 8.29289 15.2071C8.68342 15.5976 9.31658 15.5976 9.70711 15.2071L12 12.9142L14.2929 15.2071C14.6834 15.5976 15.3166 15.5976 15.7071 15.2071C16.0976 14.8166 16.0976 14.1834 15.7071 13.7929L13.4142 11.5L15.7071 9.20711C16.0976 8.81658 16.0976 8.18342 15.7071 7.79289C15.3166 7.40237 14.6834 7.40237 14.2929 7.79289L12 10.0858L9.70711 7.79289Z" fill="currentColor"></path>
+                            </svg>
+                                <span class="visually-hidden">cancel</span>
+                                </button>
+                            </div>
+                            <button class="ui-functional-button input-with-button__submit" type="submit">Start your promotion</button>
+                        </div>
                     </form>
                     <div class="heroImages mt-3">
                         <img src="public/markup/img/Group 3601.png" alt="">
@@ -23,3 +36,98 @@
     </div>
 </section>
 <div class="hero-bg-small"></div>
+<style>
+    .input-with-button {
+        display: flex;
+        padding-top: 4px;
+        padding-right: 4px;
+        padding-bottom: 4px;
+        height: 66px;
+        border-radius: 16px;
+        background-color: #fff;
+        box-shadow: 0 3px 16px rgba(110,125,177,.28);
+        transition: border-color .2s ease-out,box-shadow .2s ease-out;
+    }
+    .visually-hidden {
+        position: absolute;
+        overflow: hidden;
+        clip: rect(0 0 0 0);
+        margin: -1px;
+        padding: 0;
+        width: 1px;
+        height: 1px;
+        border: 0;
+        white-space: nowrap;
+        -webkit-clip-path: inset(100%);
+        clip-path: inset(100%);
+    }
+    .input-with-button__input-container {
+        position: relative;
+        width: 100%;
+    }
+    .input-with-button__input {
+        font-size: 16px;
+        line-height: 100%;
+        padding: 10px 40px 10px 66px;
+        width: 100%;
+        height: 100%;
+        border: 1px solid transparent;
+        border-radius: 12px;
+        background-color: #fff;
+        color: #1d1b28;
+        text-overflow: ellipsis;
+        transition: box-shadow .2s ease-out;
+    }
+    .input-with-button__cancel {
+        margin: 0;
+        padding: 0;
+        outline: none!important;
+        border: none;
+        background: none;
+        cursor: pointer;
+        color: #dee1ff;
+        transition: color .2s ease-out;
+        position: absolute;
+        top: 0;
+        right: 0;
+        display: none;
+        align-items: center;
+        width: 40px;
+        height: 100%;
+    }
+    .input-with-button__submit {
+        margin-top: 12px;
+        width: 100%;
+    }
+    .ui-functional-button {
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 140%;
+        position: relative;
+        z-index: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 16px;
+        min-height: 58px;
+        padding: 17px 30px;
+        background-color: #8e56e9;
+        background-image: linear-gradient(90deg,#a36bff 1%,#8732f4);
+        box-shadow: 0 4px 12px rgba(142,86,233,.4);
+        border: none;
+        border-radius: 12px;
+        color: #fff;
+        cursor: pointer;
+        transition: box-shadow .2s ease-out,background-color .2s ease-out,color .2s ease-out;
+    }
+    @media only screen and (min-width: 768px){
+        .input-with-button__submit {
+            flex-shrink: 0;
+            margin-top: 0;
+            min-width: 210px;
+            width: auto;
+            height: auto;
+        }
+    }
+
+</style>
